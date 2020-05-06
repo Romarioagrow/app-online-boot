4dd848d7312997f2df8bac9dcc954a8b5cc10107
Romarioagrow <romarioagrow@mail.ru>
2020-05-07 01:55:45 +0500

    Spring Boot + AngularCLI full maven build

--- src/main/java/app/onlineboot/config/WebMvcConfig.java
+++ src/main/java/app/onlineboot/config/WebMvcConfig.java
@@ -13,6 +13,7 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Bean
     public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerCustomizer() {
         return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/index.html"));
@@ -20,13 +21,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/**").setViewName("index");
+        registry.addViewController("/").setViewName("index"); /// /** does not return dist files
     }
 
     @Bean
     public ClassLoaderTemplateResolver indexHTMLTemplateResolver() {
         ClassLoaderTemplateResolver indexHTMLTemplateResolver = new ClassLoaderTemplateResolver();
-        indexHTMLTemplateResolver.setPrefix("frontend/");
+        indexHTMLTemplateResolver.setPrefix("webapp/");
         indexHTMLTemplateResolver.setSuffix(".html");
         indexHTMLTemplateResolver.setTemplateMode(TemplateMode.HTML);
         indexHTMLTemplateResolver.setCharacterEncoding("UTF-8");
